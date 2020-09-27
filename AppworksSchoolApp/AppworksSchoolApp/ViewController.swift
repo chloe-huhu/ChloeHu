8bb28cdd4e8a422702f7bbec5c838e92a88d1ef3
hu shun-ling <chloehuhu@ChloeHuHu-MacBook-Air.local>
2020-09-28 07:23:49 +0800

    week 3 作業，還未能實現stack view加背景顏色

---
+++ AppworksSchoolApp/AppworksSchoolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppworksSchoolApp
+//
+//  Created by hu shun-ling on 2020/9/27.
+//  Copyright © 2020 hu shun-ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
